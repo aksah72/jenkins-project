018250f7a3dad19af8e6b7a7570e3d27ab211445
Aman Sah <aksah72600@gmail.com>
2026-06-04 21:43:39 +0530

    Fix Jenkinsfile braces

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-res.send('Jenkins ci/cd');
+res.send('Hello world');
 });
 app.listen(3000,() => {
 console.log('Server running on 3000');
